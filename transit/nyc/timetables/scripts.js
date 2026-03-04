13da9ae1cd609abbe222772be589dbd73ef11d77
Juni <91515906+Junimeek@users.noreply.github.com>
2026-03-03 20:04:11 -0600

    march 3 2026 | added q16

--- transit/nyc/timetables/scripts.js
+++ transit/nyc/timetables/scripts.js
@@ -20,6 +20,8 @@ function GetTimetablePath(route)
 {
     switch(route)
     {
+        case "q16":
+            return "2026/03/ScheduleQ16_2025-06-29.pdf";
         case "q72":
             return "2026/02/ScheduleQ72_2026-01-04.pdf";
         default:
